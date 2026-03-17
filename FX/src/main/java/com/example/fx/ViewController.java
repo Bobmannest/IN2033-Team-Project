7c98892713d25bde42aae16ee3dcccddbbec7ab0
Kalin K <kalin.kuzev@city.ac.uk>
2026-03-17 13:31:42 +0000

    V1.4.8 - Visual improvements + CSS style class
    - Buttons react when the mouse hovers over them
    - Added CSS styleClasses for efficiency and objects that respond to mouse input

---
+++ FX/src/main/java/com/example/fx/ViewController.java
@@ -0,0 +1,17 @@
+package com.example.fx;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ViewController implements Initializable {
+    @FXML
+    Label test;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {}
+}
