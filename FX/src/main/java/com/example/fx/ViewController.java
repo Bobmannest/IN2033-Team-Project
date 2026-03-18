98d56360c12ba592384117b881336e7e3dd6c193
Kalin K <kalin.kuzev@city.ac.uk>
2026-03-18 17:49:08 +0000

    V1.5 - MySQL Database
    - Added database folder
    - Added a test schema

--- FX/src/main/java/com/example/fx/ViewController.java
+++ FX/src/main/java/com/example/fx/ViewController.java
@@ -3,6 +3,7 @@ package com.example.fx;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,5 +14,7 @@ public class ViewController implements Initializable {
     Label test;
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {}
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
 }
