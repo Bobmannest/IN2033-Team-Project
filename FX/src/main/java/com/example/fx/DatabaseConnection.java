8420d6c730ee8674b55ae26c33c9957c2653d260
LostJuro <196989663+LostJuro@users.noreply.github.com>
2026-03-25 18:36:04 +0000

    V1.5.5 - Created ipos_pu database schema and updated the DB connection

--- FX/src/main/java/com/example/fx/DatabaseConnection.java
+++ FX/src/main/java/com/example/fx/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/Test";
+    private static final String URL = "jdbc:mysql://localhost:3306/ipos_pu";
     private static final String USER = "root";
     private static final String PASS = "Ipos2026@"; // MySQL password
 
