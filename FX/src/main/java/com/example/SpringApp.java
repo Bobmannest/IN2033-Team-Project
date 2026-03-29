6a0525e1a06f22def0374f4d6030f22835db69cc
Kalin K <kalin.kuzev@city.ac.uk>
2026-03-29 20:29:57 +0100

    V1.8.2 - Better Visual Clarity

--- FX/src/main/java/com/example/SpringApp.java
+++ FX/src/main/java/com/example/SpringApp.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringApp {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
-        System.out.println("\n[Spring App started successfully]");
-        System.out.println("\nTest Email service with: curl -X POST \"http://localhost:8080/api/emails/send?recipient_email=exampleEmail@gmail.com\"\n");
+        System.out.println("\n\u001B[32m [Spring App started successfully] \u001B[0m");
+        System.out.println(" Test Email service with: curl -X POST \"http://localhost:8080/api/emails/send?recipient_email=exampleEmail@gmail.com\"\n");
     }
 }
