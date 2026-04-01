c783c50f7549582c419260b1c88423648003797e
Kalin K <kalin.kuzev@city.ac.uk>
2026-04-01 13:41:50 +0100

    V1.11 - Catalogue Item Stats
    Catalogue displays each item in the database sent by CA using the REST API dynamically and displays stats like ID, Package price, Package type and status.

--- FX/src/main/java/com/example/catalogue/CatalogueItemController.java
+++ FX/src/main/java/com/example/catalogue/CatalogueItemController.java
@@ -1,5 +1,24 @@
 package com.example.catalogue;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
 public class CatalogueItemController {
-    public void setItem(CatalogueItem item) {}
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label packageCostLabel;
+    @FXML
+    private Label packageTypeLabel;
+    @FXML
+    private Label statusLabel;
+
+    public void setItem(CatalogueItem item) {
+        idLabel.setText("#" + item.getItem_id());
+        packageCostLabel.setText("Price - £" + item.getPackage_cost());
+        packageTypeLabel.setText(item.getPackage_type());
+        statusLabel.setText("[" + item.getStatus() + "]");
+
+    }
 }
